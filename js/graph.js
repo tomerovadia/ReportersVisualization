62800473f369f7b6b9e3b73fe1f521596d25132f
Tomer Ovadia <tomertomertomer@gmail.com>
2017-08-09 14:16:30 -0400

    Create publication circles, get reporters to connect to same point

--- js/graph.js
+++ js/graph.js
@@ -9,6 +9,7 @@ export default (svg, container, width, height) => {
     "Politico": "darkred",
   }
 
+
   // var color = d3.scaleOrdinal(d3.schemeCategory10);
 
   var simulation = d3.forceSimulation()
@@ -16,7 +17,18 @@ export default (svg, container, width, height) => {
       .force("charge", d3.forceManyBody())
       .force("center", d3.forceCenter(width / 2, height / 2));
 
-  d3.json("reporters.json", function(error, graph) {
+  d3.json("data.json", function(error, graph) {
+
+    visualization.selectAll('circle')
+        .data(graph.publications, (d) => d.id)
+        .enter()
+        .append('circle')
+        .style('fill', (d) => d.color)
+        .attr('cx', (d, i) => (i+3)**5)
+        .attr('cy', 250)
+        .attr('r', 40)
+        .style('stroke', 'black')
+        .style('stroke-width', 2)
 
     if (error) throw error;
 
@@ -43,20 +55,28 @@ export default (svg, container, width, height) => {
     let samePublicationLinks = [];
     let oldPublicationLinks = [];
 
-    graph.reporters.forEach((reporter1) => {
-      graph.reporters.forEach((reporter2) => {
-        if(reporter1.id !== reporter2.id && reporter1.publication === reporter2.publication){
-          samePublicationLinks.push({"source": reporter1.id, "target": reporter2.id, "value": 250})
-        }
-      })
-    })
+    // graph.reporters.forEach((reporter1) => {
+    //   graph.reporters.forEach((reporter2) => {
+    //     if(reporter1.id !== reporter2.id && reporter1.publication === reporter2.publication){
+    //       samePublicationLinks.push({"source": reporter1.id, "target": reporter2.id, "value": 250})
+    //     }
+    //   })
+    // })
+    //
+    // graph.reporters.forEach((reporter) => {
+    //   graph.employments.forEach((employment) => {
+    //     if(reporter.publication === employment.publication){
+    //       oldPublicationLinks.push({"source": reporter.id, "target": employment.reporter, "value": 500, publication: employment.publication})
+    //     }
+    //   })
+    // })
 
     graph.reporters.forEach((reporter) => {
-      graph.employments.forEach((employment) => {
-        if(reporter.publication === employment.publication){
-          oldPublicationLinks.push({"source": reporter.id, "target": employment.reporter, "value": 500, publication: employment.publication})
-        }
-      })
+      samePublicationLinks.push({"source": reporter.id, "target": reporter.publication, "value": 250})
+    })
+
+    graph.employments.forEach((employment) => {
+      samePublicationLinks.push({"source": employment.reporter, "target": employment.publication, "value": 250})
     })
 
     let links = samePublicationLinks.concat(oldPublicationLinks);
@@ -78,7 +98,7 @@ export default (svg, container, width, height) => {
       .data(graph.reporters)
       .enter()
       .append("g")
-      .attr("class", "nodes")
+        .attr("class", "nodes")
 
     var circles = nodes
         .append("circle")
@@ -87,14 +107,15 @@ export default (svg, container, width, height) => {
           .style("stroke-width", 3)
           .attr("fill", function(d){ return `url('#${d.id}')` } )
 
+
     var reporterNames = nodes
          .append('text')
-         .text((d) => d.id)
-         .style('font-family', 'Arial')
-         .style("font-size", "12px")
-         .style("font-weight", "600")
-         .style("text-shadow", "1px 1px 2px white")
-         .attr("transform", "translate(-27,42)");
+           .text((d) => d.id)
+           .style('font-family', 'Arial')
+           .style("font-size", "12px")
+           .style("font-weight", "600")
+           .style("text-shadow", "1px 1px 2px white")
+           .attr("transform", "translate(-27,42)");
 
     nodes
           .on("mouseover", function(){
@@ -112,7 +133,7 @@ export default (svg, container, width, height) => {
         .text(function(d) { return d.id; });
 
     simulation
-        .nodes(graph.reporters)
+        .nodes(graph.reporters.concat(graph.publications))
         .on("tick", ticked);
 
     simulation.force("link")
@@ -130,9 +151,6 @@ export default (svg, container, width, height) => {
       nodes
         .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")" });
 
-      // circles
-      //   .attr("cx", function(d) { return d.x; })
-      //   .attr("cy", function(d) { return d.y; });
     }
   });
 
@@ -154,89 +172,3 @@ export default (svg, container, width, height) => {
   }
 
 }
-
-
-
-
-
-
-
-
-
-//
-//
-//
-// const d3 = require('d3');
-//
-// var svg = d3.select("svg"),
-//     width = +svg.attr("width"),
-//     height = +svg.attr("height");
-//
-// var color = d3.scaleOrdinal(d3.schemeCategory20);
-//
-// var simulation = d3.forceSimulation()
-//     .force("link", d3.forceLink().id(function(d) { return d.id; }))
-//     .force("charge", d3.forceManyBody())
-//     .force("center", d3.forceCenter(width / 2, height / 2));
-//
-// d3.json("miserables.json", function(error, graph) {
-//   if (error) throw error;
-//
-//   var link = svg.append("g")
-//       .attr("class", "links")
-//     .selectAll("line")
-//     .data(graph.links)
-//     .enter().append("line")
-//       .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
-//
-//   var node = svg.append("g")
-//       .attr("class", "nodes")
-//     .selectAll("circle")
-//     .data(graph.nodes)
-//     .enter().append("circle")
-//       .attr("r", 10)
-//       .attr("fill", function(d) { return color(d.publication); })
-//       .call(d3.drag()
-//           .on("start", dragstarted)
-//           .on("drag", dragged)
-//           .on("end", dragended));
-//
-//   node.append("title")
-//       .text(function(d) { return d.id; });
-//
-//   simulation
-//       .nodes(graph.nodes)
-//       .on("tick", ticked);
-//
-//   simulation.force("link")
-//       .links(graph.links);
-//
-//   function ticked() {
-//     link
-//         .attr("x1", function(d) { return d.source.x; })
-//         .attr("y1", function(d) { return d.source.y; })
-//         .attr("x2", function(d) { return d.target.x; })
-//         .attr("y2", function(d) { return d.target.y; });
-//
-//     node
-//         .attr("cx", function(d) { return d.x; })
-//         .attr("cy", function(d) { return d.y; });
-//   }
-// });
-//
-// function dragstarted(d) {
-//   if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-//   d.fx = d.x;
-//   d.fy = d.y;
-// }
-//
-// function dragged(d) {
-//   d.fx = d3.event.x;
-//   d.fy = d3.event.y;
-// }
-//
-// function dragended(d) {
-//   if (!d3.event.active) simulation.alphaTarget(0);
-//   d.fx = null;
-//   d.fy = null;
-// }
